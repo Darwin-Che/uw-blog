cb1b623fcb2d460c72850d7a8931a2af48bd48af
Zhaocheng Che <zhaochengche@Zhaochengs-MacBook-Pro.local>
2020-12-21 22:23:09 -0500

    rehost script and minor update

---
+++ rehost.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+usage(){
+	echo "Usage: ${0} machine_port"
+	exit 1
+}
+
+if [ ${#} -ne 1 ]; then
+	usage
+fi
+
+git pull
+
+abs_addr_machine="/Users/zhaochengche/Study/SELearn/MyProject/uw-blog/website"
+abs_addr_docker="/uw-biblog"
+container_name="uw-biblog"
+
+echo "abs_addr_machine =  " ${abs_addr_machine}
+echo "abs_addr_docker =  " ${abs_addr_docker}
+echo "container_name =  " ${container_name}
+echo "machine_port =  " ${1}
+
+
+docker rm ${container_name}
+
+docker run -ti --name ${container_name} -p ${1}:1234 -v ${abs_addr_machine}:${abs_addr_docker} http-server-base bash -c "http-server ${abs_addr_docker} -p 1234"
