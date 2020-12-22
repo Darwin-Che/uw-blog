72f68ec272a3a73dbbe1aa12e63ec95823a5167f
Zhaocheng Che <zhaochengche@Zhaochengs-MacBook-Pro.local>
2020-12-22 13:36:18 -0500

    kill container first

--- rehost.sh
+++ rehost.sh
@@ -21,7 +21,7 @@ echo "abs_addr_docker =  " ${abs_addr_docker}
 echo "container_name =  " ${container_name}
 echo "machine_port =  " ${1}
 
-
+docker kill ${container_name}
 docker rm ${container_name}
 
 docker run -ti --name ${container_name} -p ${1}:1234 -v ${abs_addr_machine}:${abs_addr_docker} ${image_name} bash -c "http-server ${abs_addr_docker} -p 1234"
